48ba9a0434fd51aadf9b90f21461a2a31767bd88
hao <qtvhao@users.noreply.github.com>
2018-07-16 00:39:42 +0700

    Update like-me.js

--- like-me.js
+++ like-me.js
@@ -1,6 +1,7 @@
 setInterval(function(){
-  if(Number(document.getElementsByClassName('recsCardboard__cards')[0].getElementsByClassName('active')[0].getElementsByClassName('recCard__info')[0].getElementsByClassName('Whs(nw)')[0].innerText.replace(/\D/gi,'')) < 27)
+  var shouldMatch = Number(document.getElementsByClassName('recsCardboard__cards')[0].getElementsByClassName('active')[0].getElementsByClassName('recCard__info')[0].getElementsByClassName('Whs(nw)')[0].innerText.replace(/\D/gi,'')) < 27;
+  if(shouldMatch)
     document.getElementsByClassName('recsGamepad__button--like')[0].click();
   else
     document.getElementsByClassName('recsGamepad__button--dislike')[0].click();
-}, 1234)
+}, 1234);
