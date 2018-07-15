37176514789157bf0472943ab746c312314d38d2
hao <qtvhao@users.noreply.github.com>
2018-07-16 00:38:19 +0700

    Update like-me.js

--- like-me.js
+++ like-me.js
@@ -1,1 +1,6 @@
-
+setInterval(function(){
+  if(Number(document.getElementsByClassName('recsCardboard__cards')[0].getElementsByClassName('active')[0].getElementsByClassName('recCard__info')[0].getElementsByClassName('Whs(nw)')[0].innerText.replace(/\D/gi,'')) < 27)
+    document.getElementsByClassName('recsGamepad__button--like')[0].click();
+  else
+    document.getElementsByClassName('recsGamepad__button--dislike')[0].click();
+}, 1234)
